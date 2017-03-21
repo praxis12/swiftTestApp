96ffd4f73c9641b24f2f322e9f64d5faab35316d
Josh Arntz <jarntz12@gmail.com>
2017-03-21 12:56:15 -0500

    First commit

--- Swift Test 1/ViewController.swift
+++ Swift Test 1/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var tapCount = 0
+    @IBOutlet weak var firstLabel: UILabel!
 
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
+   
+    @IBAction func buttonHellYesTapped(_ sender: Any) {
+        outputLabel.text = " 8=======D~~~"
+        tapCount += 1
+        print(tapCount)
+        if tapCount > 10 {
+            firstLabel.text = "You really want that D"
+        }
+    }
+    
+    @IBAction func yesButtonPress(_ sender: Any) {
+        firstLabel.text = "You want it more than that... \n Now tell me, do you want some dick?"
+        print("Yes was clicked")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
